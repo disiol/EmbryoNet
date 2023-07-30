4c3e5d48ead38d7e06939be4551f61b13c872431
disiol <denisimus_games@ukr.net>
2023-07-30 20:37:44 +0300

    SetCurrentDetection

---
+++ Assets/Scripts/RotationManager/RotationController.cs
@@ -0,0 +1,100 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace RotationManager
+{
+    public class RotationController : MonoBehaviour
+    {
+        // Initial rotation values
+        [HideInInspector] public Vector3 previousRotation = Vector3.zero;
+
+        // Current rotation values
+        [HideInInspector] public Vector3 currentRotation = Vector3.zero;
+
+        // Reference to UI buttons for increasing/decreasing rotation
+        [Header("RotationX")] [SerializeField] private Button increaseXButton;
+        [SerializeField] private TMP_InputField menuXInput;
+        [SerializeField] private Button decreaseXButton;
+
+        [Header("RotationY")] [SerializeField] private Button increaseYButton;
+        [SerializeField] private TMP_InputField menuYInput;
+        [SerializeField] private Button decreaseYButton;
+
+        [Header("RotationZ")] [SerializeField] private Button increaseZButton;
+        [SerializeField] private TMP_InputField menuZInput;
+        [SerializeField] private Button decreaseZButton;
+
+        [Header("Rotation")] [SerializeField] private float currentRotationStep;
+        [SerializeField] private float minRotation;
+        [SerializeField] private float maxRotation;
+
+        [HideInInspector] public RotationManager rotationManager;
+
+        private void Start()
+        {
+            // Add click listeners to the buttons
+            increaseXButton.onClick.AddListener(IncreaseXRotation);
+            decreaseXButton.onClick.AddListener(DecreaseXRotation);
+
+            increaseYButton.onClick.AddListener(IncreaseYRotation);
+            decreaseYButton.onClick.AddListener(DecreaseYRotation);
+
+            increaseZButton.onClick.AddListener(IncreaseZRotation);
+            decreaseZButton.onClick.AddListener(DecreaseZRotation);
+        }
+
+        // Update rotation values for each axis
+        private void IncreaseXRotation()
+        {
+            currentRotation.x += currentRotationStep;
+            UpdateRotation();
+        }
+
+        private void DecreaseXRotation()
+        {
+            currentRotation.x -= currentRotationStep;
+            UpdateRotation();
+        }
+
+        private void IncreaseYRotation()
+        {
+            currentRotation.y += currentRotationStep;
+            UpdateRotation();
+        }
+
+        private void DecreaseYRotation()
+        {
+            currentRotation.y -= currentRotationStep;
+            UpdateRotation();
+        }
+
+        private void IncreaseZRotation()
+        {
+            currentRotation.z += currentRotationStep;
+            UpdateRotation();
+        }
+
+        private void DecreaseZRotation()
+        {
+            currentRotation.z -= currentRotationStep;
+            UpdateRotation();
+        }
+
+        // Apply the 0-360 degree constraint and update the object's rotation
+        private void UpdateRotation()
+        {
+            currentRotation.x = Mathf.Clamp(currentRotation.x, minRotation, maxRotation);
+            menuXInput.text = currentRotation.x.ToString();
+
+            currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation, maxRotation);
+            menuYInput.text = currentRotation.y.ToString();
+
+            currentRotation.z = Mathf.Clamp(currentRotation.z, minRotation, maxRotation);
+            menuZInput.text = currentRotation.z.ToString();
+            
+            rotationManager.UpdateRotation();
+        }
+    }
+}
